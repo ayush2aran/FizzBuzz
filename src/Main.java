e95daad16c1abb6cf88ddad6d87c9fdcd1838247
Ayush Aran <ayush22ar@gmail.com>
2023-09-15 00:42:28 -0400

    experimenting method calls

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        String[] str = new String[]{"trmp"};
+        FizzBuzz.main(str);
+    }
+}
